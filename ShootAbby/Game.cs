f9d84f326ea068d768ad13f408addb4f876faf7f
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2024-09-24 19:55:58 +0200

    feat (game): affichage avec timer

---
+++ ShootAbby/Game.cs
@@ -0,0 +1,14 @@
+namespace ShootAbby
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+        private void NewFrame(object sender, EventArgs e)
+        {
+            Console.WriteLine("c");
+        }
+    }
+}
