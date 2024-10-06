4a692232c387887eaf0bd0345c22b0a86f9cbaae
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2024-10-06 17:38:59 +0200

    feat(projectil): créer le projectile

---
+++ ShootAbby/Model/Projectil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootAbby
+{
+    public partial class Projectil
+    {
+        private int _x;              //position axe x
+        private int _y;              //position axe y
+        private int _deplacementX;   //déplacement selon axe x
+        private int _deplacementY;   //déplacement selon axe y
+        private Rectangle _rectangle;
+
+        public int X { get => _x; set => _x = value; }
+        public int Y { get => _y; set => _y = value; }
+        public Rectangle Rectangle { get => _rectangle; set => _rectangle = value; }
+        public int DeplacementX { get => _deplacementX; set => _deplacementX = value; }
+        public int DeplacementY { get => _deplacementY; set => _deplacementY = value; }
+
+        public Projectil(int x, int y, int depX, int depY) 
+        { 
+            _x = x;
+            _y = y;
+            _deplacementX = depX;
+            _deplacementY = depY;
+            _rectangle = new Rectangle(X, Y, 20, 20);
+        }
+    }
+}
---
+++ ShootAbby/View/Projectil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootAbby
+{
+    public partial class Projectil
+    {
+        //le stylo nommée witchBush pour écrire dans l'affichage
+        private Pen witchBrush = new Pen(new SolidBrush(Color.Black), 3);
+
+        /// <summary>
+        /// Forme du rocher
+        /// </summary>
+        /// <param name="drawingGame"></param>
+        public void Render(BufferedGraphics drawingGame)    //zone de dessin = Buffergraphics
+        {
+            drawingGame.Graphics.DrawRectangle(witchBrush, Rectangle);
+        }
+    }
+}
