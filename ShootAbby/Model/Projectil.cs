7d51a048bfa80cfe8e9feb8aa190ec9cdee44bff
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2024-10-06 17:47:45 +0200

    feat(Game): assignes le constructeur du projectiles aux touches

--- ShootAbby/Model/Projectil.cs
+++ ShootAbby/Model/Projectil.cs
@@ -8,25 +8,24 @@ namespace ShootAbby
 {
     public partial class Projectil
     {
-        private int _x;              //position axe x
-        private int _y;              //position axe y
         private int _deplacementX;   //déplacement selon axe x
         private int _deplacementY;   //déplacement selon axe y
         private Rectangle _rectangle;
 
-        public int X { get => _x; set => _x = value; }
-        public int Y { get => _y; set => _y = value; }
         public Rectangle Rectangle { get => _rectangle; set => _rectangle = value; }
         public int DeplacementX { get => _deplacementX; set => _deplacementX = value; }
         public int DeplacementY { get => _deplacementY; set => _deplacementY = value; }
 
         public Projectil(int x, int y, int depX, int depY) 
         { 
-            _x = x;
-            _y = y;
             _deplacementX = depX;
             _deplacementY = depY;
-            _rectangle = new Rectangle(X, Y, 20, 20);
+            _rectangle = new Rectangle(x, y, 20, 20);
+        }
+        public void MoveProjectile()
+        {
+            _rectangle.X += _deplacementX;
+            _rectangle.Y += _deplacementY;
         }
     }
 }
